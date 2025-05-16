993f3cb58692e9bd2fb329d8c044fc48a374255f
matsumura-kengo-9735 <598-student007@la-study.com>
2025-05-16 09:15:46 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev9735MatsumuraKengoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev9735MatsumuraKengoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev9735MatsumuraKengoApplication.class, args);
+	}
+
+}
